ef3fc54b8129ca49cfedbda59278721bb89a6713
Akash Palrecha <akashpalrecha@gmail.com>
2021-02-05 19:36:41 +0530

    initial commit

---
+++ tfCode.js
@@ -0,0 +1,17 @@
+// jshint esversion:9
+
+function updateLogStatus(text){
+  console.log(text);
+  let list = document.getElementById("logs");
+  let litem = document.createElement("li");
+  let textContent = document.createTextNode(text);
+  litem.appendChild(textContent);
+  list.appendChild(litem);
+}
+
+const x = tf.cast(tf.ones([4,4,3]), 'int32').add(254);
+// console.log(x);
+updateLogStatus(x);
+updateLogStatus("Lol");
+const y = tf.image.resizeBilinear(x, [2,2]);
+updateLogStatus(y);
